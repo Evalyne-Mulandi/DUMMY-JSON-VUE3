0c569b13ae200313a8530da14e1b069bf12a666a
Evalyne-Mulandi <eva.nthenya.mulandi@gmail.com>
2022-09-22 17:18:27 +0300

    dummy project

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+   
+<div> 
+<Form/>
+<Resources/>
+</div>
+   
+</template>
+
+<script>
+import Form from "./components/Form.vue";
+import Resources from "./components/resources.vue";
+
+
+
+export default {
+  name: 'App',
+  components: {
+    Form,
+    Resources
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/resources.vue
@@ -0,0 +1,33 @@
+<template>
+  
+  <main>
+    
+  </main>
+</template>
+
+<script>
+export default {
+    name: 'resources',
+    data (){
+      return{
+             
+        }
+    },
+    mounted(){
+    this.fetchAllProducts()
+    },
+    methods:{
+      async fetchAllProducts(){
+        const res=await fetch("https://dummyjson.com/products")
+        const json=await res.json()
+        console.log(json);
+
+      }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
